65badfc74ea2b2c101947c9edd58f1e70debec79
Shaofeng Zhu <shaozhu@microsoft.com>
2017-07-25 10:47:49 -0700

    Fix buildofficeapibundle.bat to switch to correct directory

--- buildofficeapibundle.bat
+++ buildofficeapibundle.bat
@@ -3,7 +3,7 @@ rd /s /q %_targetdir%
 call node %SRCROOT%\richapi\Tools\buildofficeapinpm.js %_targetdir%
 pushd %_targetdir%
 call npm link
-cd %~dp0
+cd /d %~dp0
 call npm link @microsoft/office-api
 call node_modules\.bin\webpack
 copy %~dp0dist\app.bundle.js %TARGETROOT%\x64\debug\richapi\x-none\app.bundle.js
